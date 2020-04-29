be784115f5dd8534b6e2ee68cd3a0c2194c60fd6
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-29 12:33:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   home: {
   team_name: "Brooklyn Nets",
   colors: ["Black", "White"],
-  players: {player_name: "Alan Anderson",
+  players: [ {player_name: "Alan Anderson",
   number: 0,
   shoe: 16,
   points: 22,
@@ -13,7 +13,7 @@ def game_hash
   assists: 12,
   steals: 3,
   blocks: 1,
-  slam_dunks: 1,
+  slam_dunks: 1
   },
   {player_name: "Reggie Evans",
   number: 30,
@@ -23,7 +23,7 @@ def game_hash
   assists: 12,
   steals: 12,
   blocks: 12,
-  slam_dunks: 7,
+  slam_dunks: 7
   }, 
   {player_name: "Brook Lopez",
   number: 11,
@@ -33,7 +33,7 @@ def game_hash
   assists: 10,
   steals: 3,
   blocks: 1,
-  slam_dunks: 15,
+  slam_dunks: 15
   },
   {player_name: "Mason Plumlee",
   number: 1,
@@ -43,7 +43,7 @@ def game_hash
   assists: 6,
   steals: 3,
   blocks: 8,
-  slam_dunks: 5,
+  slam_dunks: 5
   },
   {player_name: "Jason Terry",
   number: 31,
@@ -53,11 +53,11 @@ def game_hash
   assists: 2,
   steals: 4,
   blocks: 11,
-  slam_dunks: 1,
+  slam_dunks: 1
   }
   },
   
-  {away: { 
+  away: { 
   team_name: "Charlotte Hornets",
   colors: ["Turquoise", "Purple"],
   players: {player_name: "Jeff Adrien",
@@ -68,7 +68,7 @@ def game_hash
   assists: 1,
   steals: 2,
   blocks: 7,
-  slam_dunks: 2,
+  slam_dunks: 2
   },
   {player_name: "Bismack Biyombo",
   number: 0,
@@ -78,7 +78,7 @@ def game_hash
   assists: 7,
   steals: 22,
   blocks: 15,
-  slam_dunks: 10,
+  slam_dunks: 10
   }, 
   {player_name: "DeSagna Diop",
   number: 2,
@@ -88,7 +88,7 @@ def game_hash
   assists: 12,
   steals: 4,
   blocks: 5,
-  slam_dunks: 5,
+  slam_dunks: 5
   },
   {player_name: "Ben Gordon",
   number: 8,
@@ -98,7 +98,7 @@ def game_hash
   assists: 2,
   steals: 1,
   blocks: 1,
-  slam_dunks: 0,
+  slam_dunks: 0
   },
   {player_name: "Kemba Walker",
   number: 33,
@@ -108,9 +108,8 @@ def game_hash
   assists: 12,
   steals: 7,
   blocks: 5,
-  slam_dunks: 12,
-  }
-  }
+  slam_dunks: 12
+  }]
 }
 }
 end
